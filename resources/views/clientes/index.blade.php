6c513f2db874866870b3430041c93986ba0d33e0
Asis-hub <asis.reyes@hotmail.com>
2023-09-05 11:13:46 -0600

    Tabla con clientes por nombre y apellidos

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,27 +1,38 @@
 @extends('layouts.app')
 @section('title', $viewData["title"])
 @section('content')
-<div class="row" >
+<div class="row">
     <div class="col-md-6 col-lg-4 mb-2">
         <a class="button" href="{{ route('clientes.form') }}">Registrar cliente</a>
     </div>
 </div>
 
 <div class="row">
-            @foreach ($viewData['clientes']->reverse() as $cliente)
-                <div class="col-md-4 col-lg-3 mb-2">
-                    <div class="card">
-                        <div class="card-body text-center">
-                            <a href="{{ route('clientes.show', ['id' => $cliente->getId()]) }}"
-                                class="btn bg-primary text-white">Cliente:
-                                {{ $cliente->getNombre() }}
-                                {{ $cliente->getPrimerApellido() }}
-                                {{ $cliente->getSegundoApellido() }}
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
+    <div class="col-md-12">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Primer Apellido</th>
+                    <th>Segundo Apellido</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($viewData['clientes'] as $cliente)
+                <tr>
+                    <td>{{ $cliente->getNombre() }}</td>
+                    <td>{{ $cliente->getPrimerApellido() }}</td>
+                    <td>{{ $cliente->getSegundoApellido() }}</td>
+                    <td>
+                        <a href="{{ route('clientes.show', ['id' => $cliente->getId()]) }}" class="btn bg-primary text-white">Ver</a>
+                        <!-- Add other actions buttons as needed -->
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @endsection
